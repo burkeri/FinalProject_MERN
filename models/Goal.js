a25ae78551ec5ea99d9bec777d80155942d8ce2f
Winfield H <winterfreshness@gmail.com>
2019-01-24 11:22:57 -0500

    Created Goal model.

---
+++ models/Goal.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create the schema
+const goalSchema = new Schema({
+    goalID: {type: Number, required: true, unique: true},
+    userID: {type: Number, required: true},
+    category: {type: String, required: true},
+    icon: {type: String, required: true},
+    name: {type: String, required: true},
+    frequency: {type: Number, required: true},
+    description: String,
+    progress: {type: Number, default: 0},
+    totalWeeks: {type: Number, default: 0},
+    streak: {type: Number, default: 0},
+    createdAt: {type: Date, default: Date.now},
+    lastUpdated: {type: Date, default: Date.now}
+});
+
+const Goal = mongoose.model('Goal', goalSchema);
+
+module.exports = Goal;

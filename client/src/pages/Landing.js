944009f3ad7002ad437357f045d0f3cfeaee7982
Winfield H <winterfreshness@gmail.com>
2019-01-23 23:16:59 -0500

    Installed styled-components. Tested on landing page.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React from "react";
 import { Col, Row, Container } from "../components/Grid";
+import styled from "styled-components";
 
+// CSS in JS components =======================
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => (props.primary ? "palevioletred" : "white")};
+  color: ${props => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const Link = styled.a`
+  color: black;
+  font-weight: 200;
+`;
+// =============================================
+
+// Landing component
 function Landing() {
   return (
-    <Container>
+    <Wrapper>
       <Row>
         <Col size="md-12">
           <div className="jumbotron mt-2 mb-2 border shadow">
-            <h1 className="text-center">Landing Page</h1>
+            <Title>Landing Page</Title>
             <hr />
-            <p><a href="/signup">Sign Up</a></p>
-            <a href="/login">Login</a>
-          </div>  
+            <Button primary>
+              <Link href="/signUp">Sign Up</Link>
+            </Button>
+            <Button>
+              <Link href="/login">Login</Link>
+            </Button>
+          </div>
         </Col>
       </Row>
-    </Container>
+    </Wrapper>
   );
 }
 

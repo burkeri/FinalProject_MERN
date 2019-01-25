4d2e7d9270c77982f03bc8559661295ce33faba9
Richelle Burke <burkeri@alumni.upenn.edu>
2019-01-25 13:37:27 -0500

    commit form validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,11 @@ module.exports = {
     registerUser: function(req, res) {
         console.log("server: ");
         console.log(req.body);
-        
+
+        // take in req.body and make sure that you don't have
+        // multiple users by checking against the database, 
+        // if it does not exist,
+        // hash the password
+        // then save the new user to the database
     }
 };

65321c50532c7277bc88161725afa57ad2d5c148
Richelle Burke <burkeri@alumni.upenn.edu>
2019-01-28 00:45:00 -0500

    tinkering with login

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const path = require("path");
 const router = require("express").Router();
 const passport = require("passport");
-const local = require("passport-local");
+const { ensureAuthenticated } = require("../config/auth");
 const userController = require("../controllers/userController");
 
 // user -register
@@ -20,11 +20,17 @@ router.post("/user/login", (req, res, next) => {
   });
 
 // user -logout
-router.get('/user/logout', (req, res) => {
+router.get('/logout', (req, res) => {
   req.logout();
-  res.redirect('/user/login');
+  res.redirect('/');
   console.log('logged out');
 });
+
+// dashboard
+router.get("/user/dashboard", ensureAuthenticated, (req, res) => {
+  res.send(req.user.username);
+});
+  
 
 // send all requests to react app
 router.use((req, res) => {

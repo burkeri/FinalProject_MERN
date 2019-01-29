864bc0bcecec8e0920b0e24a355a138488b345f6
Winfield H <winterfreshness@gmail.com>
2019-01-29 12:55:15 -0500

    Build for Heroku

---
+++ client/build/precache-manifest.42e1ed3da5adbb43423250bcef168325.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea16879a655832d1f161",
+    "url": "/static/js/main.ea16879a.chunk.js"
+  },
+  {
+    "revision": "66a300ebddaeb47a8372",
+    "url": "/static/js/1.66a300eb.chunk.js"
+  },
+  {
+    "revision": "66a300ebddaeb47a8372",
+    "url": "/static/css/1.52426ae0.chunk.css"
+  },
+  {
+    "revision": "62479aca9a961476e0f9d6e55f3b8158",
+    "url": "/index.html"
+  }
+];

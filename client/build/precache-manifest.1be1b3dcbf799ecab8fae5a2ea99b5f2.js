3449d1a5a0c7bb58da6d83b8b1d279f40d4efafd
Winfield H <winterfreshness@gmail.com>
2019-01-30 13:33:27 -0500

    Removed the protected dashboard route

---
+++ client/build/precache-manifest.1be1b3dcbf799ecab8fae5a2ea99b5f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89a7a98936cefd2783ba",
+    "url": "/static/js/main.89a7a989.chunk.js"
+  },
+  {
+    "revision": "508a6be9c3bda8759ded",
+    "url": "/static/js/1.508a6be9.chunk.js"
+  },
+  {
+    "revision": "508a6be9c3bda8759ded",
+    "url": "/static/css/1.52426ae0.chunk.css"
+  },
+  {
+    "revision": "01bde189051e1c73791279e33b834fb1",
+    "url": "/index.html"
+  }
+];

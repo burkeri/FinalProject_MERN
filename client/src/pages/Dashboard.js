e5545daee7e635b7467dfa2937aa17c32e540901
Richelle Burke <burkeri@alumni.upenn.edu>
2019-01-31 11:53:29 -0500

    commiting dashboard styles for push

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import "./dashboard.css";
-import MaterialIcon from 'material-icons-react';
+import MaterialIcon from "material-icons-react";
 import {
   Container,
   Row,
   Col,
   Card,
+  CardBody,
   CardTitle,
   CardText,
   CardImg,
@@ -14,7 +15,7 @@ import {
   Button,
   ListGroup,
   ListGroupItem,
-  ListGroupItemHeading, 
+  ListGroupItemHeading,
   ListGroupItemText
 } from "reactstrap";
 
@@ -49,29 +50,25 @@ class Dashboard extends Component {
       .catch(err => console.log(err));
   };
 
-  handleDeleteBook = (id) => {
+  handleDeleteBook = id => {
     // console.log(`Goal ID to delete: ${id}`);
     API.deleteGoal(id)
       .then(res => this.getGoals())
       .catch(err => console.log(err));
-  }
+  };
 
   handleUpdateGoal = (id, prog) => {
     console.log(`Goal ID to update: ${id}`);
     console.log(`Goal's progress: ${prog}`);
     API.updateGoal(id, prog + 1)
-    .then(res => this.getGoals())
-    .catch(err => console.log(err));
-  }
+      .then(res => this.getGoals())
+      .catch(err => console.log(err));
+  };
 
   render() {
-
     return (
       <Container className="allBackground">
-        <Row>
-          <Col>
-
-          {/* Row for the dropdown menu
+        {/* Row for the dropdown menu
           <Row>
             <Col xs={{ size: "auto", offset: 10 }}>
               <Dropdown
@@ -91,124 +88,88 @@ class Dashboard extends Component {
             </Col>
           </Row> */}
 
+        {/* Main Image and Name */}
+        <Row className="header">
+          <Col>
+            <h1 className="text-center" id="welcomeUser">
+              {this.state.username}
+            </h1>
+            <div id="profilePic">
+              <MaterialIcon icon="account_circle" id="profileIcon" />
+            </div>
+          </Col>
+        </Row>
 
-          {/* Main Image and Name */}
-          <Row className="header">
-            <Col>
-              <h1 className="text-center" id="welcomeUser">{this.state.username}</h1>
-              <div id="profilePic">
-                <MaterialIcon icon="account_circle" id="profileIcon"/>
-              </div>
-            </Col>
-          </Row>
-
-          {/* Social Ticker */}
-          <Row >
-            <Col>
-              <Card id="ticker">
-                  {/* <CardTitle className="font-weight-bold">Another User</CardTitle> */}
-                  <CardText>
-                    <h3 id="text">
-                    "If you want to accomplish the goals of your 
-                    life, you have to begin with the spirit."
-                    <br></br>
-                    <hr></hr>
-                    <br></br>
-                    Oprah Winfrey 
-                    </h3>
-                  </CardText>
-              </Card>
-            </Col>
-          </Row>
-
-          {/* Goal header and button */}
-          <Row>
-            <Col>
-              <p className="text-center text-white">Goals:</p>
-              <div className="text-center mb-2">
-                <Button color="danger" href="/addgoalcreate">
-                  Add Goal
-                </Button>
-              </div>
-            </Col>
-          </Row>
-          {/* Goal list */}
-          <Row>
-            <Col>
-              {goals.length ? (
-                <ListGroup>
-                  {goals.map(goal => 
-                    <ListGroupItem key={goal._id}>
-                      <ListGroupItemHeading>
-                        {goal.name}
-                        <Button 
-                          color="danger" 
-                          className="float-right"
-                          onClick={() => this.handleDeleteBook(goal._id)}
-                        >X</Button>
-                      </ListGroupItemHeading>
-                      <ListGroupItemText>
-                        Frequency: {goal.frequency} times per week
-                        <br />
-                        Progress: {(goal.progress / goal.frequency)} %
-                      </ListGroupItemText>
-                    </ListGroupItem>
-                  )}
-                </ListGroup>
-              ) : (<h2 className="text-white">No goals...</h2>)}
-            </Col>
-          </Row>
-
-        {/* Goal header and button */}
+        {/* Social Ticker */}
         <Row>
           <Col>
-            <p className="text-center text-white">Goals:</p>
+            <Card id="ticker">
+              <CardBody>
+                <CardText>
+                  <h3 id="text">
+                    "If you want to accomplish the goals of your life, you have to
+                    begin with the spirit."
+                    <br />
+                    <hr />
+                    <br />
+                    Oprah Winfrey
+                  </h3>
+                </CardText>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+
+        {/* Goal header and button */}
+        <Row id="goalHeader">
+          <Col>
+            <p className="text-center">Goals:</p>
+            <hr></hr>
             <div className="text-center mb-2">
-              <Button color="danger" href="/addgoalcreate">
+              <Button id="addGoal" href="/addgoalcreate">
                 Add Goal
               </Button>
             </div>
           </Col>
         </Row>
+
         {/* Goal list */}
         <Row>
           <Col>
-              <ListGroup>
-                {this.state.goals.map(goal => 
-                  <ListGroupItem key={goal._id}>
-                    <ListGroupItemHeading>
-                      <Button 
-                        outline
-                        size="lg"
+            <ListGroup>
+              {this.state.goals.map(goal => (
+                <ListGroupItem key={goal._id} id="goalItem">
+                  <ListGroupItemHeading>
+                    <Button id="goalIcon">
+                      <i className={goal.icon} />
+                    </Button>
+                    {goal.name}
+                    <Button
+                      close
+                      color="danger"
+                      className="float-right"
+                      onClick={() => this.handleDeleteBook(goal._id)}
+                    />
+                  </ListGroupItemHeading>
+                  <ListGroupItemText>
+                    Frequency: {goal.frequency} times per week
+                    <br />
+                    Progress: {(goal.progress / goal.frequency) * 100} %
+                  </ListGroupItemText>
+                  <Button
+                      id="finishGoal"
+                      onClick={() =>
+                        this.handleUpdateGoal(goal._id, goal.progress)
+                      }
+                    >
+                      Finish
+                    </Button>
 
-                        >
-                        <i className={goal.icon} />
-                      </Button>
-                      {goal.name}
-                      <Button
-                        close 
-                        color="danger" 
-                        className="float-right"
-                        onClick={() => this.handleDeleteBook(goal._id)}
-                      />
-                    </ListGroupItemHeading>
-                    <ListGroupItemText>
-                      Frequency: {goal.frequency} times per week
-                      <br />
-                      Progress: {(goal.progress / goal.frequency) * 100} %
-                    </ListGroupItemText>
-                    <div className="text-center mb-2">
-                      <Button
-                        color="success"
-                        onClick={() => this.handleUpdateGoal(goal._id, goal.progress)}
-                      >Finish</Button>
-                    </div>
-                  </ListGroupItem>
-                )}
-              </ListGroup>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
           </Col>
         </Row>
-        
       </Container>
     );
   }

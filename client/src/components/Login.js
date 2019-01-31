1010d679831cff94d2cb09e025744499fd283dea
Richelle Burke <burkeri@alumni.upenn.edu>
2019-01-31 14:26:18 -0500

    login style started

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,30 +1,37 @@
 import React, { Component } from "react";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from "reactstrap";
+import "./login.css";
 
 export class Login extends Component {
   render() {
     return (
-      <Form action="/user/login" method="POST">
-        <FormGroup>
-          <Label for="usernameLogin">Username</Label>
-          <Input
-            className="form-control"
-            type="text"
-            name="username"
-            id="usernameLogin"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="passowrdLogin">Password</Label>
-          <Input
-            className="form-control"
-            type="password"
-            name="password"
-            id="passwordLogin"
-          />
-        </FormGroup>
-        <Button type="submit">Login</Button>
-      </Form>
+      <Container id="loginBackground">
+        <Row>
+          <Col>
+            <Form action="/user/login" method="POST">
+              <FormGroup>
+                <Label for="usernameLogin">Username</Label>
+                <Input
+                  className="form-control"
+                  type="text"
+                  name="username"
+                  id="usernameLogin"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="passowrdLogin">Password</Label>
+                <Input
+                  className="form-control"
+                  type="password"
+                  name="password"
+                  id="passwordLogin"
+                />
+              </FormGroup>
+              <Button type="submit">Login</Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }

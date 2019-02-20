15aaeae12504affe4d29dbaa5c22ed75ec0c927c
Richelle Burke <burkeri@alumni.upenn.edu>
2019-02-20 18:20:25 -0500

    ran build

---
+++ client/build/precache-manifest.8ec0c0af93c79c2ee54eec7d6dce3c60.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a17c3a46a0870b8da7bb",
+    "url": "/static/css/main.7349a8d1.chunk.css"
+  },
+  {
+    "revision": "a17c3a46a0870b8da7bb",
+    "url": "/static/js/main.a17c3a46.chunk.js"
+  },
+  {
+    "revision": "5a75165d6dd93c9de272",
+    "url": "/static/css/1.fd58824d.chunk.css"
+  },
+  {
+    "revision": "5a75165d6dd93c9de272",
+    "url": "/static/js/1.5a75165d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e5539dcf6082a05879d9a26211fb0bc4",
+    "url": "/static/media/fontIconPicker.e5539dcf.svg"
+  },
+  {
+    "revision": "8c2c34ece6757b8eace3fdcb7939d021",
+    "url": "/static/media/fontIconPicker.8c2c34ec.woff"
+  },
+  {
+    "revision": "a86caa7efa7c4269ec646a4dce6c6e92",
+    "url": "/static/media/fontIconPicker.a86caa7e.ttf"
+  },
+  {
+    "revision": "1c4284ac69057b34be4ccaae10efe4b4",
+    "url": "/index.html"
+  }
+];

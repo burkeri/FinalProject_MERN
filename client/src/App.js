aa25b49af422244d5473344f53af2bd0870376a5
Winfield H <winterfreshness@gmail.com>
2019-03-12 22:46:59 -0400

    Stash files

--- client/src/App.js
+++ client/src/App.js
@@ -173,8 +173,8 @@ class App extends Component {
               render={() => (
                 <Details
                   userChoiceID={this.state.userChoiceID}
-                  username={this.state.username}
                   userChoiceGoal={this.state.userChoiceGoal}
+                  username={this.state.username}
                   getGoals={this.getGoals}
                 />
               )}
